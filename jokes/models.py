0c82642d67b9f345c3df873b3a55268e3d07edaa
heather-irene <cunningham.heatherirene@gmail.com>
2025-04-08 16:53:38 -0400

    1st-branch: Added Joke model, ran makemigrations and migrate

--- jokes/models.py
+++ jokes/models.py
@@ -1,3 +1,15 @@
 from django.db import models
 
 # Create your models here.
+## BEGIN class
+class Joke(models.Model):
+    question = models.TextField(max_length=200)
+    answer = models.TextField(max_length=100, blank=True)
+    created = models.DateTimeField(auto_now_add=True)
+    updated = models.DateTimeField(auto_now=True)
+
+    
+    def __str__(self):
+        """" To string """
+        return self.question
+## END class
