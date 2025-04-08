8fba095aeaebb6783e7f5745a6fa4433bf638da4
heather-irene <cunningham.heatherirene@gmail.com>
2025-04-08 17:20:05 -0400

    1st-branch: Added Joke ListView, joke_list html template, url routing path, and added link to base html nav

--- jokes/urls.py
+++ jokes/urls.py
@@ -1,9 +1,12 @@
 from django.urls import path
+from .views import JokeListView
 
 
 app_name = "jokes"
 
 
 urlpatterns = [
-    
+    ## Remember only URL paths beginning with '/jokes/' will be handed off to the URLConf 
+    ## of the jokes app, so "" will actually be "/jokes/".
+    path("", JokeListView.as_view(), name="list"),
 ]
--- jokes/views.py
+++ jokes/views.py
@@ -1,3 +1,16 @@
-from django.shortcuts import render
+from django.views.generic import ListView
+from .models import Joke
+
 
 # Create your views here.
+class JokeListView(ListView):
+    ## A minimal ListView is incredibly simple. 
+    ## It just requires the model to query (ex. below):
+    model = Joke
+    ## Notice, above, you do not define a template_name attribute as you did with HomePageView. 
+    ## When no template_name is defined for a ListView, Django infers a template_name as: 
+    #### `<app_name>/<model>_list.html`
+    ## ...Where <app_name> is the name of the app (i.e., jokes). 
+    ## And, <model> is the lowercase name of the model. 
+    ## So, for JokeListView, Django is looking for the template at: 
+    #### `jokes/joke_list.html`
