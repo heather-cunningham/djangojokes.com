66010a61fd008bb2067122cf68a23094a4d82450
heather-irene <cunningham.heatherirene@gmail.com>
2025-04-15 17:01:44 -0400

    send-email-sendgrid: Send email utils fcn

---
+++ common/utils/email.py
@@ -0,0 +1,17 @@
+import sendgrid
+from sendgrid.helpers.mail import Mail
+from django.conf import settings
+
+
+def send_email(to, subject, content, sender="admin@example.com"): 
+    ## Change the sender email to the one reg'd w/ Twilio-SendGrid (i.e., your gmail)
+    ## to actually send any email   
+    sender_client = sendgrid.SendGridAPIClient(settings.SENDGRID_API_KEY)
+    mail = Mail(
+        from_email=sender,
+        to_emails=to,
+        subject=subject,
+        html_content=content
+    )
+    return sender_client.send(mail)
+     
