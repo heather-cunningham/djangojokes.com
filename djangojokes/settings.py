59d919f741ff4f91a69edf4a8d9c0b12e19fe94b
heather-irene <cunningham.heatherirene@gmail.com>
2025-04-15 16:30:46 -0400

    send-email-sendgrid: SendGrid API and settings and sent one practice email

--- djangojokes/settings.py
+++ djangojokes/settings.py
@@ -9,7 +9,7 @@ https://docs.djangoproject.com/en/5.2/topics/settings/
 For the full list of settings and their values, see
 https://docs.djangoproject.com/en/5.2/ref/settings/
 """
-
+import os
 from pathlib import Path
 
 # Build paths inside the project like this: BASE_DIR / 'subdir'.
@@ -148,3 +148,9 @@ STATICFILES_DIRS = [
 # https://docs.djangoproject.com/en/5.2/ref/settings/#default-auto-field
 
 DEFAULT_AUTO_FIELD = 'django.db.models.BigAutoField'
+
+
+## MUST stay at bottom of this file
+if (os.environ.get('ENVIRONMENT') != 'production'):
+    from .local_settings import *
+## DON'T PUT ANYTHING BELOW THIS
