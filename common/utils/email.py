7ed8a4d37b800eac7d098e0480262c375bdac980
heather-irene <cunningham.heatherirene@gmail.com>
2025-04-16 11:48:53 -0400

    forms-widgets: Jobs app:     is_form_valid(self, form) fcn and tested manually, but have not rcvd email yet.  Some emails from yesterday came in, in the wee hrs of the AM last night

--- common/utils/email.py
+++ common/utils/email.py
@@ -3,7 +3,7 @@ from sendgrid.helpers.mail import Mail
 from django.conf import settings
 
 
-def send_email(to, subject, content, sender="admin@example.com"): 
+def send_email(to, subject, content, sender="cunningham.heatherirene@gmail.com"): 
     ## Change the sender email to the one reg'd w/ Twilio-SendGrid (i.e., your gmail)
     ## to actually send any email   
     sender_client = sendgrid.SendGridAPIClient(settings.SENDGRID_API_KEY)
@@ -14,4 +14,4 @@ def send_email(to, subject, content, sender="admin@example.com"):
         html_content=content
     )
     return sender_client.send(mail)
-     
+    
--- jobs/views.py
+++ jobs/views.py
@@ -1,5 +1,7 @@
+import html
 from django.urls import reverse_lazy
 from django.views.generic import FormView, TemplateView
+from common.utils.email import send_email
 from .forms import JobApplicationForm
 
 
@@ -7,6 +9,23 @@ class JobApplicationView(FormView):
     template_name = "jobs/joke_writer.html"
     form_class = JobApplicationForm
     success_url = reverse_lazy("jobs:thanks")
+
+
+    def is_form_valid(self, form):
+        data = form.cleaned_data
+        to = 'cunningham.heatherirene@gmail.com'
+        subject = 'Application for Joke Writer'
+        content = f'''<p>Hey HR Manager!</p>
+            <p>Job application received:</p>
+            <ol>'''
+        for key, value in data.items():
+            label = key.replace('_', ' ').title()
+            entry = html.escape(str(value), quote=False)
+            content += f'<li>{label}: {entry}</li>'
+        content += '</ol>'
+        send_email(to, subject, content)
+        return super().is_form_valid(form)
+## END JobApplicationView class
 
 
 class JobApplicationThanksView(TemplateView):
