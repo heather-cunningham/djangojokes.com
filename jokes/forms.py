565ffd91d54b455898b930b55088b0fa4d6c9d98
heather-irene <cunningham.heatherirene@gmail.com>
2025-04-17 12:34:19 -0400

    forms-widgets: Added tag field to Joke form, db, models, etc.

--- jokes/forms.py
+++ jokes/forms.py
@@ -5,12 +5,13 @@ from .models import Joke
 class JokeForm(ModelForm):
     class Meta:
         model = Joke
-        fields = ["question", "answer", "category", ]
+        fields = ["question", "answer", "category", "tags"]
         widgets = {
             "question": Textarea(attrs={'cols': 80, 'rows': 3, 'autofocus': True}),
             "answer": Textarea(attrs={'cols': 80, 'rows': 2, 'placeholder': 'Make it funny!', 
                                       'required': True}),
         }
         help_texts = {
-            'question': 'No dirty jokes please.'
+            'question': 'No dirty jokes please.',
+            'tags': 'Use Ctrl-click to select multiple tags.'
         }
