36cf81d89da27d2ee0048cbabc1d0c9341431654
heather-irene <cunningham.heatherirene@gmail.com>
2025-04-23 10:37:14 -0400

    msgs-framework: Example of msgs in base template and AboutUs page

--- pages/views.py
+++ pages/views.py
@@ -1,3 +1,4 @@
+from django.contrib import messages
 ## By default, render is imported from django.shortcuts. 
 ## But, it's a more common practice to use Django’s built-in generic views, which are class-based. 
 ## So, replace the import below:
@@ -5,10 +6,22 @@
 ## With ...
 from django.views.generic import TemplateView
 
+
 # Create your views here.
 class HomePageView(TemplateView):
     template_name = 'pages/home.html'
 
 
+## BEGIN
 class AboutPageView(TemplateView):
     template_name = 'pages/about.html'
+
+
+    def get(self, request, *args, **kwargs):
+        messages.debug(request, 'Debug message.')
+        messages.info(request, 'Info message.')
+        messages.success(request, 'Success message.')
+        messages.warning(request, 'Warning message.')
+        messages.error(request, 'Error message.')
+        return super().get(request, args, kwargs)
+## END class
