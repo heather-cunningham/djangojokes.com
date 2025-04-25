5991cbc8b1a999c709742e415141fbf522a79e76
heather-irene <cunningham.heatherirene@gmail.com>
2025-04-25 15:46:10 -0400

    queries-ajax-view-fcns:  vote fcnal View: Updated joke_detail.py to incld vote View and AJAX-JS fcns

--- jokes/views.py
+++ jokes/views.py
@@ -147,7 +147,7 @@ def vote(request, slug):
             joke_vote = JokeVote.objects.get(user=user, joke=joke)
 
             if (joke_vote.vote == vote): # User's new vote is the same as old vote.
-                msg = 'Right. You told us already. Geez.'
+                msg = 'Right. You told us already.'
             else: # User changed vote.
                 joke_vote.vote = vote # Update JokeVote instance.
                 joke_vote.save() # Save.
