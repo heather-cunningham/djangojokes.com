c30384ef0da81a1b591fc09e8606955dc2bfaebe
heather-irene <cunningham.heatherirene@gmail.com>
2025-05-05 11:57:45 -0400

    custom-django-admin: Custom site tab, title, and header for the Django Admin site

--- common/admin.py
+++ common/admin.py
@@ -1,3 +1,8 @@
 from django.contrib import admin
 
-# Register your models here.
+## Django Admin custom changes
+#### These can be changed in any app’s admin.py file, but it makes sense to put them in the
+#### common app.  
+admin.site.index_title = "Home" # index.html / Homepage
+admin.site.site_title = "Django Jokes Admin"
+admin.site.site_header = "Django Jokes Admin"
